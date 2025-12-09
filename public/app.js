777e2d166e4ffc2d6799034253bc0ecc533f4696
CosteGieF <costegief@users.noreply.github.com>
2025-12-09 15:47:05 +1300

    Enable barge-in: keep mic streaming during assistant speech

--- public/app.js
+++ public/app.js
@@ -17,7 +17,6 @@ class VoiceAssistant {
         this.audioPlaybackQueue = [];
         this.nextPlaybackTime = 0;
         this.isAssistantSpeaking = false;
-        this.micPaused = false;
 
         // Reconnection logic
         this.reconnectAttempts = 0;
@@ -203,11 +202,6 @@ class VoiceAssistant {
             return;
         }
 
-        // Pause mic streaming when assistant is speaking to avoid echo/overlap
-        if (this.micPaused) {
-            return;
-        }
-
         // Queue the chunk
         this.pendingAudioChunks.push(data);
 
@@ -288,14 +282,8 @@ class VoiceAssistant {
                 case 'response.audio.delta':
                     // Play audio response
                     if (data.delta) {
-                        this.micPaused = true; // pause mic while assistant speaks
                         this.playAudio(data.delta);
                     }
-                    break;
-
-                case 'response.audio.done':
-                    // Assistant finished speaking
-                    this.micPaused = false;
                     break;
 
                 case 'error':
